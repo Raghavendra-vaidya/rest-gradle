d3b25fa2ff2241b9fac3b8de9f5cc251156bdd17
Raghavendra Vaidya <rkvaidya58@gmail.com>
2022-07-28 10:36:46 +0530

    init suite :tada:

---
+++ src/test/java/com/rv/Tests/Test1.java
@@ -0,0 +1,11 @@
+package com.rv.Tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test1 {
+    @Test
+    public void a1(){
+        Assert.assertTrue(true);
+    }
+}
