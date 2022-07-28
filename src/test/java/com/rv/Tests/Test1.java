0e62a8aaaf21bf82146e81deaa57468d6213972c
Raghavendra Vaidya <rkvaidya58@gmail.com>
2022-07-28 11:24:34 +0530

    chore(suite): sample data :zap:

--- src/test/java/com/rv/Tests/Test1.java
+++ src/test/java/com/rv/Tests/Test1.java
@@ -1,11 +1,45 @@
 package com.rv.Tests;
 
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
+
 public class Test1 {
+
+    @BeforeClass
+    public void setUPUrl(){
+        RestAssured.baseURI = "https://petstore.swagger.io";
+        RestAssured.basePath ="/v2/pet/";
+    }
     @Test
     public void a1(){
         Assert.assertTrue(true);
+    }
+
+    @Test
+    public void a2(){
+        String endPoint = "https://petstore.swagger.io/v2/pet/findByStatus?status=available";
+        String ep = RestAssured.baseURI+RestAssured.basePath+"findByStatus?status=available";
+
+        Response response = given().log().ifValidationFails()
+                .header("accept","application/json")
+                .when()
+                    .get("findByStatus?status=available")
+                .then().
+                    assertThat().statusCode(200).extract().response();
+
+        JsonPath api = response.jsonPath();
+        System.out.println(response.body().asString());
+        System.out.println(RestAssured.baseURI);
+        System.out.println(ep);
     }
 }
