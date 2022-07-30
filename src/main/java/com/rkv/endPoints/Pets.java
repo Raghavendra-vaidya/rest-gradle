66356347603545cefe35a0574dccc87534465dba
Raghavendra vaidya <raghavendra@spotdraft.com>
2022-07-30 15:16:18 +0530

    chore(suite): add pets endpoint methods :zap: (#3)

--- src/main/java/com/rkv/endPoints/Pets.java
+++ src/main/java/com/rkv/endPoints/Pets.java
@@ -1,4 +1,115 @@
 package com.rkv.endPoints;
 
+import com.rkv.utility.Configuration;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+
 public class Pets {
+    static Configuration config = new Configuration();
+
+    public static Response addPet(String body, int statusCode) {
+        RestAssured.baseURI = config.base_uri;
+        String endPoint = RestAssured.baseURI + config.pets_ep;
+        Response response =
+                given()
+                        .log().ifValidationFails()
+                        .header("Content-Type", "application/json")
+                        .accept("*/*")
+                        .body(body)
+                .when()
+                        .post(endPoint)
+                .then()
+                        .assertThat().statusCode(statusCode).extract().response();
+
+        return response;
+    }
+
+    public static Response updatePet(String body, int statusCode) {
+        RestAssured.baseURI = config.base_uri;
+        String endPoint = RestAssured.baseURI + config.pets_ep;
+        Response response =
+                given()
+                        .log().ifValidationFails()
+                        .header("Content-Type","application/json")
+                        .accept("*/*")
+                        .body(body)
+                .when()
+                        .put(endPoint)
+                .then()
+                        .assertThat().statusCode(statusCode).extract().response();
+
+        return response;
+    }
+
+    public static Response uploadPetImage(int petID,String filePath, int statusCode) {
+        RestAssured.baseURI = config.base_uri;
+        String endPoint = RestAssured.baseURI +config.pets_ep+"/"+petID+config.pets_image_ep;
+        Response response =
+                given()
+                        .log().ifValidationFails()
+                        .header("Content-Type","multipart/form-data")
+                        .accept("*/*")
+                        .multiPart("file",new File(filePath))
+                .when()
+                        .post(endPoint)
+                .then()
+                        .assertThat().statusCode(statusCode).extract().response();
+
+        return response;
+    }
+
+    public static Response findPetByStatus(String petStatus, int statusCode){
+        RestAssured.baseURI = config.base_uri;
+        String endPoint = RestAssured.baseURI+config.findByStatus;
+        Response response =
+                given()
+                        .log().ifValidationFails()
+                        .header("Content-Type","application/json")
+                        .accept("*/*")
+                        .queryParam("status",petStatus)
+                .when()
+                        .get(endPoint)
+                .then()
+                        .assertThat().statusCode(statusCode).extract().response();
+
+        return response;
+    }
+
+    public static Response getPetById(String petID, int statusCode){
+        RestAssured.baseURI = config.base_uri;
+        String endpoint = RestAssured.baseURI+config.pets_ep+"/"+petID;
+
+        Response response =
+                given()
+                        .log().ifValidationFails()
+                        .header("Content-Type","application/json")
+                        .accept("*/*")
+                .when()
+                        .get(endpoint)
+                .then()
+                        .assertThat().statusCode(statusCode).extract().response();
+
+        return response;
+    }
+
+    public static Response deletePetById(String petID, int statusCode){
+        RestAssured.baseURI = config.base_uri;
+        String endpoint = RestAssured.baseURI+config.pets_ep+"/"+petID;
+
+        Response response =
+                given()
+                        .log().ifValidationFails()
+                        .header("Content-Type","application/json")
+                        .accept("*/*")
+                .when()
+                        .delete(endpoint)
+                .then()
+                        .assertThat().statusCode(statusCode).extract().response();
+
+        return response;
+    }
 }
---
+++ src/main/java/com/rkv/utility/Configuration.java
@@ -0,0 +1,29 @@
+package com.rkv.utility;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class Configuration {
+    public  String base_uri ;
+    public String pets_ep;
+
+    public String pets_image_ep;
+
+    public String findByStatus;
+
+    public Configuration() {
+        Properties propApi = new Properties();
+        Properties propConfig = new Properties();
+        try {
+            propApi.load(new FileInputStream("src/main/resources/api.properties"));
+            propConfig.load(new FileInputStream("src/main/resources/config.properties"));
+            this.base_uri = propConfig.getProperty("BASE_URI");
+            this.pets_ep = propApi.getProperty("PET");
+            this.findByStatus = propApi.getProperty("PET_BY_STATUS");
+            this.pets_image_ep = propApi.getProperty("UPLOAD_IMAGE");
+
+        } catch (Exception e) { e.printStackTrace();}
+    }
+
+
+}
