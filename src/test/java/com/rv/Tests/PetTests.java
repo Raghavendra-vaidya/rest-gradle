1c12848218348357d51573561aade8a5276b5d31
Raghavendra Vaidya <raghavendra@spotdraft.com>
2022-08-07 19:17:59 +0530

    update pet tests :white_check_mark:

--- src/test/java/com/rv/Tests/PetTests.java
+++ src/test/java/com/rv/Tests/PetTests.java
@@ -6,6 +6,8 @@ import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class PetTests {
 
     @Test(description = "create new pet and verify")
@@ -18,15 +20,79 @@ public class PetTests {
         Assert.assertEquals(Astatus, "available");
     }
 
+    @Test(description = "find pet by status available")
+    public void findPetByStatusAvailable(){
+        String status = "available";
+        Response apiResponse = Pets.findPetByStatus(status,200);
+        List<String> listOfStatus=  apiResponse.jsonPath().get("status");
+        boolean isAvailable = true;
+        for (String s: listOfStatus) {
+            if (s.equalsIgnoreCase("available")){
+                continue;
+            }
+            else {
+                isAvailable = false;
+                break;
+            }
+        }
+        Assert.assertTrue(isAvailable, "All the pets are not of status available in api response");
+    }
+    @Test(description = "find pet by status pending")
+    public void findPetByStatusPending(){
+        String status = "pending";
+        Response apiResponse = Pets.findPetByStatus(status,200);
+        List<String> listOfStatus=  apiResponse.jsonPath().get("status");
+
+        boolean isAvailable = true;
+        for (String s: listOfStatus) {
+            if (s.equalsIgnoreCase("pending")){
+                continue;
+            }
+            else {
+                isAvailable = false;
+                break;
+            }
+        }
+        Assert.assertTrue(isAvailable, "All the pets are not of status pending in api response");
+    }
+
+    @Test(description = "find pet by status sold")
+    public void findPetByStatusSold(){
+        String status = "sold";
+        Response apiResponse = Pets.findPetByStatus(status,200);
+        List<String> listOfStatus=  apiResponse.jsonPath().get("status");
+
+        boolean isAvailable = true;
+        for (String s: listOfStatus) {
+            if (s.equalsIgnoreCase("sold")){
+                continue;
+            }
+            else {
+                isAvailable = false;
+                break;
+            }
+        }
+        Assert.assertTrue(isAvailable, "All the pets are not of status pending in api response");
+    }
     @Test(description = "get pet by id and verify")
     public void getPetByID(){
         String requestBody = Helpers.formSampleCreatePetReqBody();
         Response apiResponse = Pets.addPet(requestBody, 200);
         String petID = apiResponse.jsonPath().get("id").toString();
         Response response = Pets.getPetById(petID,200);
-        System.out.println(response.body().asString());
         Assert.assertEquals(response.jsonPath().get("id").toString(), petID);
     }
+
+    @Test(description = "delete created pet and verify")
+    public void deleteNewPet(){
+        String requestBody = Helpers.formSampleCreatePetReqBody();
+        Response apiResponse = Pets.addPet(requestBody, 200);
+        String petId = apiResponse.jsonPath().get("id").toString();
+
+        Pets.deletePetById(petId, 200);
+    }
+
+
 
     @Test(description = "This excluded", groups = {"defect"})
     public void boomTest(){
