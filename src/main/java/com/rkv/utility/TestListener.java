0f93c9ab7894fa61248f713ff13a4fa7b31d4a65
Raghavendra Vaidya <raghavendra@spotdraft.com>
2022-08-07 19:53:07 +0530

    implement extent reports 4 :spark:

---
+++ src/main/java/com/rkv/utility/TestListener.java
@@ -0,0 +1,48 @@
+package com.rkv.utility;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+
+public class TestListener implements ITestListener {
+    @Override
+    public void onTestStart(ITestResult result) {
+
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+
+    }
+
+    @Override
+    public void onTestFailedWithTimeout(ITestResult result) {
+
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+
+    }
+}
